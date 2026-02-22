54dafa575b989bf69b7d9a6706deef02c6af3742
Nakata Saki <amethyst225crystal@gmail.com>
2026-02-22 12:54:33 +0900

    MyBatisでStudentのCRUD処理を実装

---
+++ src/main/java/raisetech/StudentManagement2026ver/Application.java
@@ -0,0 +1,51 @@
+package raisetech.StudentManagement2026ver;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+  @Autowired
+  private StudentRepository repository;
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @GetMapping("/students")
+  public List<Student> getStudents() {
+    return repository.findAll();
+  }
+
+  @GetMapping("/student")
+  public String getStudent(@RequestParam String name) {
+    Student student = repository.searchByName(name);
+    return student.getName() + "さん " + student.getAge() + "歳";
+  }
+
+  @PostMapping("/student")
+  public void registerStudent(String name, int age) {
+    repository.registerStudent(name, age);
+  }
+
+  @PatchMapping("/student")
+  public void updateStudent(int age, String name) {
+    repository.updateStudent(age, name);
+  }
+
+  @DeleteMapping("/student")
+  public void deleteStudent(String name) {
+    repository.deleteStudent(name);
+  }
+
+}
